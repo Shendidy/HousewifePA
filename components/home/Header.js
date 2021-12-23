fd98e26db85f4d53361ba01302d8eb79ab352f05
Shendidy <sherif@shendidy.com>
2021-12-23 13:29:27 +0000

    completed header component

--- components/home/Header.js
+++ components/home/Header.js
@@ -10,12 +10,65 @@ const Header = () => {
                     source={require('../../assets/images/name_logo_white_004i.png')} 
                 />
             </TouchableOpacity>
+
+            <View style={styles.iconsContainer}>
+                <TouchableOpacity>
+                    <View style={styles.totalCartsBadge}>
+                        <Text style={styles.totalCartsBadgeText}>5</Text>
+                    </View>
+                    <Image
+                        source={require('../../assets/images/cart_plus_icon.png')}
+                        style={styles.icon}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Image
+                        source={require('../../assets/images/store_icon.png')}
+                        style={styles.icon}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Image
+                        source={require('../../assets/images/exit_icon.png')}
+                        style={styles.icon}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    totalCartsBadge: {
+        backgroundColor: 'black',
+        borderColor: 'red',
+        borderWidth: 1,
+        position: 'absolute',
+        left: 25,
+        bottom: 20,
+        width: 25,
+        height: 18,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 100
+    },
+    totalCartsBadgeText: {
+        color: 'white',
+        fontWeight: '600'
+    },
+    icon: {
+        width: 24,
+        height: 24,
+        marginLeft: 10,
+        resizeMode: 'contain'
+    },
+    iconsContainer: {
+        flexDirection: 'row'
+    },
+    container: {
         justifyContent: 'space-between',
         alignItems: 'center',
         flexDirection: 'row',
